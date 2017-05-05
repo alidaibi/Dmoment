8de0207ebc95ca1d65e0328d85d7e97d633580b6
alidaibi <seven@alidaibi.com>
2017-05-05 17:44:36 +0800

    v1.0.3
    支持天数添加
    	支持月份添加
    	支持年数添加
    	支持时分秒添加

--- Dmoment.js
+++ Dmoment.js
@@ -1,34 +1,38 @@
 (function(window,factory){
-	var df = factory(window,window.document);
-	window.df = df;
+	var dmoment = factory(window,window.document);
+	window.dmoment = dmoment;
 	if(typeof module == 'object' && module.exports){
-		module.exports = df;
+		module.exports = dmoment;
 	}
 }(window, function l(window,document){
 	'use strict';
 	
 	function hooks(ops){
 		/*
-			µ÷ÓÃµÄÊ±ºòops¿É·ÅÈëÒÔÏÂ²ÎÊı
+			è°ƒç”¨çš„æ—¶å€™opså¯æ”¾å…¥ä»¥ä¸‹å‚æ•°
 			new Date(...)
-			'Tue Apr 25 2017 14:28:07 GMT+0800 (ÖĞ¹ú±ê×¼Ê±¼ä)'
+			'Tue Apr 25 2017 14:28:07 GMT+0800 (ä¸­å›½æ ‡å‡†æ—¶é—´)'
 			{
-				//configÅäÖÃ....
+				//configé…ç½®....
 				locale: 'en',
 				dateformat: 'yyyy-MM-dd',
 				date: '2015-5-5'
 			}
 		 */
-		DmomentConfig(ops);
-		return new Dmoment();
+		
+		var dmoment = dmoment || new Dmoment();
+		DmomentConfig(ops,dmoment);
+		return dmoment;
 	}
 	function Dmoment(){
 
 	}
+	//var dmoment = null;
+
 	var dateConf = {
-		locale: 'cn', //µØÇøÓïÑÔ
-		dateformat: 'yyyy-MM-dd', //Ä¬ÈÏ¸ñÊ½
-		date: new Date() //ÈÕÆÚÊ±¼ä
+		locale: 'cn', //åœ°åŒºè¯­è¨€
+		dateformat: 'yyyy-MM-dd', //é»˜è®¤æ ¼å¼
+		date: new Date() //æ—¥æœŸæ—¶é—´
 	}
 	var _toString = Object.prototype.toString;
 	var formatTokens = /[Hh]mm(ss)?|MM?M?M?|DD?D?D?|ddd?d?|yyyy|YYYY|YY?|hh?|HH?|mm?|ss?|S{1,9}|./g;
@@ -43,26 +47,40 @@
 	function isDate(obj){
 		return _toString.call(obj) === '[object Date]';
 	}
-	function isNumber(obj){
-		return toString.call(obj) ==='[object Number]';
-	}
 	function dateFormat(format){
 		format = format || dateConf.dateformat;
-		var arr = format.match(formatTokens);
-		console.log(arr);
+		formatFunctions[format] = formatFunctions[format] || makeFormatFunction(format);
+		return formatFunctions[format](this);
 	}
-	function DmomentConfig(ops){
+	function DmomentConfig(ops,dmoment){
 		ops = ops || dateConf.date;
 		if(!isDate(ops) && isObject(ops)){
 			CopyConfig(ops);
 		}else{
 			!isDate(ops) && ops.replace(/-/g,'/');
-			dateConf.date = new Date(ops);
+			dmoment._d = dateConf.date = new Date(ops);
 		}
 	}
 	function zeroFill(number, targetLength, forceSign){
-		console.log(number, targetLength, forceSign);
+		var absNumber = '' + Math.abs(number);
+		var zeroToFill = absNumber.length - targetLength;
+		return (zeroToFill ? '0' : '') + number;
+	}
+	function makeFormatFunction(format){
+		var arr = format.match(formatTokens);
+		for(var i = 0,length = arr.length; i < length; i++){
+			if(formatFunctions[arr[i]]){
+				arr[i] = formatFunctions[arr[i]];
+			}
+		}
 
+		return function(dmom){
+			var output = '', i = 0;
+			for(i = 0;i<length;i++){
+				output += isFunction(arr[i])? arr[i].call(dmom,format) : arr[i];
+			}
+			return output;
+		}
 	}
 	function addFormatToken(token, padded, ordinal, callback){
 		var func = callback;
@@ -75,22 +93,74 @@
 			formatFunctions[token] = func;
 		}
 		if(padded){
-			formatTokenFunctions[padded[0]] = function(){
+			formatFunctions[padded[0]] = function(){
 				return zeroFill(func.apply(this,arguments), padded[1],padded[2]);
 			}
 		}
 	}
+	function makeGetSet(unit){
+		return function(value){
+			if(value){
+				return set(this, unit, value);
+			}else{
+				return get(this, unit);
+			}
+		}
+	}
+	function getSetMonth(value){
+		if(value){
+			return set(this,'Month',value);
+		}
+		return get(this, 'Month') + 1;
+	}
 	function get(dmom, unit){
-		return dmom.date['get' + unit]() || NaN;
+		return dmom._d['get' + unit]() || NaN;
+	}
+	function set(dmom,unit,value){
+		return dmom._d['set' + unit](dmom._d['get' + unit]() + value) || NaN;
 	}
 	function CopyConfig(ops){
 		for(var key in ops){
 			dateConf[key] = ops[key];
 		}
 	}
+	var add = createAddFunction(1,'add');
+	var subtract = createAddFunction(-1, 'subtract');
+
+	function createAddFunction(direction, name){
+		return function(val, period){
+			if(val === null || period ===null){
+				console.log('é”™è¯¯');
+			}
+			var obj = { value: val, period: period};
+			addSubtract(this, obj, direction);
+			return this;
+		}
+	}
+	function addSubtract(dmom, dateObj, isAdding){
+		dmom[dateObj.period](dateObj.value * isAdding);
+
+	}
+	addFormatToken(0, ['yyyy', 4], 0, 'year');
 	addFormatToken(0, ['YYYY', 4], 0, 'year');
-	var proto = Dmoment.prototype;
-	proto.format = dateFormat;
+	addFormatToken(0, ['dd', 2], 0, 'date');
+	addFormatToken(0, ['MM', 2], 0, 'month');
+	addFormatToken(0, ['hh', 2], 0 , 'hour');
+	addFormatToken(0, ['mm', 2], 0 , 'minutes');
+	addFormatToken(0, ['ss', 2], 0 , 'seconds');
+
+	var proto       = Dmoment.prototype;
+	proto.format    = dateFormat;
+	proto.year      = proto.years   = makeGetSet('FullYear');
+	proto.month     = proto.months  = getSetMonth;
+	proto.date      = makeGetSet('Date');
+	proto.day       = proto.days    = makeGetSet('Day');
+	proto.hour      = proto.hours   = makeGetSet('Hours');
+	proto.minutes   = makeGetSet('Minutes');
+	proto.seconds   =makeGetSet('Seconds');
+	proto.add       = add;
+	proto.subtract  = subtract;
+
 
 	hooks.prototype = proto;
 	return hooks;
